81bff42823d1b20046bcf5af516829a9307b385e
julian porrovecchio <julian.porrovecchio@hotmail.com>
2024-07-05 21:40:14 -0300

    Preentrega1+Porrovecchio

--- trivia.js
+++ trivia.js
@@ -1,49 +1,3 @@
-// let nombre = "juan"
-
-// console.log(nombre);
-
-// let nombre2 = "pedro"
-// console.log(nombre2);
-
-// let nombre3 = nombre + " " + nombre2    
-
-// console.log(nombre3);
-
-// if(nombre==="juan"){
-//     nombre2="marcos" 
-//     nombre3= nombre + " " +nombre2
-// } 
-// console.log(nombre2);
-// console.log(nombre3);
-
-// let numeroUsuario = Number(prompt("ingrese un numero"))
-
-// console.log(numeroUsuario);
-
-// if(numeroUsuario>=1){   
-// numeroUsuario= numeroUsuario + 50 
-
-// numeroUsuario= numeroUsuario / 2
-// alert("Tu Numero +50 /2 = "+ numeroUsuario )
-// }else{
-//     alert("Te equivocaste")
-// }
-
-
-
-// if(numeroUsuario===8 || numeroUsuario=== 14){
-//     alert("ganaste un premio")
-// }else{
-//     alert("segui participando")
-// }
-
-
-// for(let i = 10; i<=10;i++){
-
-//     let resultado = numeroUsuario * i;
-//     console.log(numeroUsuario+ " x " + i + " = " + resultado);
-// }
-
 alert("Bienvenido a la Trivia Sobre Cultura General!")
 
 alert("El Objetivo del juego es responder preguntas eligiendo la opcion correcta")
@@ -54,30 +8,117 @@ alert("Comenzemos!")
 
 let nombreUsuario = prompt("ingresa tu nombre")
 
-let correcta = 3
-let incorrecta = -1
-let puntuacion = 0  
+console.log(nombreUsuario);
 
-console.log(puntuacion);
+var puntuacion = 0
+
+var correctas = 0
+
+//El problema que tuve era que estaba nombrando las variables con let, lo cual no me permitia que el puntaje se actualize en cada if y se mofique de manera global. Por eso cambie las variables a var.
 
 let respuesta1= prompt("En que año Finalizo la Segunda Guerral Mundial? A. 1950 B. 1960 C. 1945 D. 1934")
 
-console.log(respuesta1);
+if(respuesta1=="C"||respuesta1=="c"){
 
-function sumar () {
-    puntuacion= puntuacion+3
+  puntuacion= puntuacion+3
+
+  correctas= correctas + 1
+
+  puntuacionA="correcta"
+
+}else {
+  puntuacion=puntuacion - 1
+
+  puntuacionA="incorrecta"
+}
+console.log(puntuacion);
+
+let respuesta2=prompt("Cuantos copas del mundo tiene la selección de Argentina? A. 5 B. 1 C. 8 D. 3")
+
+if(respuesta2=="D"||respuesta2=="d"){
+
+  puntuacion= puntuacion+3
+
+  correctas=correctas+1 
+
+}else {
+
+  puntuacion=puntuacion - 1
+
 }
 
-if(respuesta1=="c"){
-    sumar
+console.log(puntuacion);
+
+let respuesta3=prompt("Quien escribio la Odisea? A. Homero B. Shakespeare C. Picasso D. Aristoteles")
+
+if(respuesta3=="A"||respuesta3=="a"){
+
+  puntuacion= puntuacion+3
+
+  correctas=correctas+1
+
+}else {
+
+  puntuacion=puntuacion - 1
+  
+}
+console.log(puntuacion);
+
+let respuesta4=prompt("En que continente se encuentra Ecuador? A. Europa B. America C. Asia D. Oceanía")
+
+if(respuesta4=="B"||respuesta4=="b"){
+
+  puntuacion= puntuacion+3
+
+  correctas=correctas+1
+
+}else {
+
+  puntuacion=puntuacion - 1
+
+}
+console.log(puntuacion);
+
+let respuesta5=prompt("Cual es la moneda del Reino Unido? A. Libra B. Peso C. Dolar D. Euro")
+
+if(respuesta5=="A"||respuesta5=="a"){
+
+  puntuacion= puntuacion+3
+
+  correctas=correctas+1
+  
+}else {
+
+  puntuacion=puntuacion - 1
+  
+}
+
+console.log("puntuacion total: "+puntuacion);
+
+console.log("respuestas correctas: " + correctas);
+
+//No pude encontrar una aplicacion realmente util para un ciclo en el juego que elegi. Por eso hice un ciclo for que se repite una vez para ejecutar el alert de los puntos obtenidos y para que se vea el entendimiento del concepto del bucle. 
+
+for(var i = 0; i < 1;i++){
+  if (correctas==1){
+    alert("Has obtenido: -1 puntos!")
     
-    console.log(puntuacion);
-}else{
-  alert("incorrecto")
-
+  }else if(correctas==2){
+    alert("Has obtenido: 3 puntos!")
+    
+  }else if(correctas==3){
+    alert("Has obtenido: 7 puntos!")
+    
+  }else if (correctas==4){
+    alert("Has obtenido: 11 puntos!")
+    
+  }else if(correctas==5){
+    alert("Has obtenido: 15 puntos!")
+    
+  }
+  else{
+    alert("No has obtenido ningun punto")
+  }
+  
 }
-
-
-
-
 
